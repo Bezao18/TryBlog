1320ff4fd875fe875c98dc9f7ff45860d54f193a
Bernardo <bernardo.r.s18@gmail.com>
2022-12-13 16:48:24 -0300

    Implementa autenticação em Program.cs

--- src/TryBlog/Program.cs
+++ src/TryBlog/Program.cs
@@ -13,6 +13,23 @@ builder.Services.AddScoped<ITryBlogRepository, TryBlogRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("SECRETKEYYYYYYYY"))
+    };
+});
 
 var app = builder.Build();
 
